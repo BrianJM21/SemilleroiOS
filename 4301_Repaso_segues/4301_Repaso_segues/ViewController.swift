935847518e4485d2705e272bb0cd685a8e68f823
BrianJM21 <brianjmoedano@gmail.com>
2022-12-28 12:10:37 -0600

    Clase Segues automáticos y manuales

---
+++ 4301_Repaso_segues/4301_Repaso_segues/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  4301_Repaso_segues
+//
+//  Created by User on 28/12/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    
+    @IBAction func myUnwindAction(unwindSegue: UIStoryboardSegue) {
+        
+    }
+
+
+}
+
